20b949204e6a5cc2b2e964aba23b1b574efa6d93
Juan Rodriguez <jsrd98@gmail.com>
2018-11-18 09:56:30 -0500

    [feat]: Send notification method added

---
+++ api/v1/users/controller.js
@@ -0,0 +1,70 @@
+const {
+  db,
+  uploadFile,
+  getOrCreateUser,
+  deleteFile,
+  sendNotification,
+} = require('../../../lib/firebase');
+
+module.exports.all = async (req, res, next) => {
+  try {
+    const usersSnapshot = await db.collection('users').get();
+    res.json(usersSnapshot.docs.map(doc => ({
+      id: doc.id,
+      data: doc.data(),
+    })));
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports.read = async (req, res, next) => {
+  const { user } = req;
+  if (!user && !user.id) next(new Error('Bad request'));
+  res.json({ data: user });
+};
+
+module.exports.upload = async (req, res, next) => {
+  try {
+    const { files, user } = req;
+    const profilePicture = await uploadFile(files[0], next);
+    if (profilePicture instanceof Error) next(profilePicture);
+    const userInfo = await getOrCreateUser(user);
+    if (userInfo instanceof Error) next(userInfo);
+    if (!user.id) Object.assign(user, userInfo);
+    if (user.profile_picture) await deleteFile(user.profile_picture.ref);
+    await db.collection('users')
+      .doc(user.id)
+      .update({
+        profilePicture,
+      });
+    res.json({ data: { profilePicture } });
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports.update = async (req, res, next) => {
+  const { user, body } = req;
+  if (!body) next(new Error('Bad request'));
+  try {
+    if (!user.id) user.id = await getOrCreateUser(user).id;
+    await db.collection('users')
+      .doc(user.id)
+      .update(body);
+    res.json({ data: body });
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports.sendNotification = (req, res, next) => {
+  const { body } = req;
+  sendNotification('switch', body)
+    .then((response) => {
+      res.json({ data: { response } });
+    })
+    .catch((e) => {
+      next(e);
+    });
+};
--- api/v1/users/routes.js
+++ api/v1/users/routes.js
@@ -1,84 +1,20 @@
 const { Router } = require('express');
-const {
-  db,
-  uploadFile,
-  getUser,
-  deleteFile,
-} = require('../../../lib/firebase');
 
 const router = Router();
+const controller = require('./controller');
+const { isACompany } = require('../../../lib/auth');
 
-router.get('/all', async (req, res, next) => {
-  try {
-    const usersSnapshot = await db.collection('users').get();
-    res.json(usersSnapshot.docs.map(doc => ({
-      id: doc.id,
-      data: doc.data(),
-    })));
-  } catch (e) {
-    next(e);
-  }
-});
+// router
+//   .param('id', controller.id);
 
-router.get('/', async (req, res, next) => {
-  const { user } = req;
-  if (!user && !user.id) next(new Error('Bad request'));
-  res.json({ data: user });
-});
 
-router.post('/signup', async (req, res, next) => {
-  const { user, body } = req;
-  if (!body) next(new Error('Bad request'));
-  try {
-    if (user.id) {
-      res.json({ data: user });
-    } else {
-      Object.assign(body, {
-        uid: user.uid,
-        phone_number: user.phone_number,
-      });
-      await db.collection('users')
-        .doc()
-        .set(body, { merge: true });
-      res.json({ data: body });
-    }
-  } catch (e) {
-    next(e);
-  }
-});
+router.route('/')
+  .get(controller.read)
+  .put(controller.update);
 
-router.post('/upload', async (req, res, next) => {
-  try {
-    const { files, user } = req;
-    const profilePicture = await uploadFile(files[0], next);
-    if (profilePicture instanceof Error) next(profilePicture);
-    const userInfo = await getUser(user);
-    if (userInfo instanceof Error) next(userInfo);
-    if (!user.id) Object.assign(user, userInfo);
-    if (user.profile_picture) await deleteFile(user.profile_picture.ref);
-    await db.collection('users')
-      .doc(user.id)
-      .update({
-        profile_picture: profilePicture,
-      });
-    res.json({ data: { profile_picture: profilePicture } });
-  } catch (e) {
-    next(e);
-  }
-});
+router.get('/all', controller.all);
+router.post('/upload', controller.upload);
 
-router.put('/', async (req, res, next) => {
-  const { user, body } = req;
-  if (!body) next(new Error('Bad request'));
-  try {
-    if (!user.id) user.id = await getUser(user).id;
-    await db.collection('users')
-      .doc(user.id)
-      .update(body);
-    res.json({ data: body });
-  } catch (e) {
-    next(e);
-  }
-});
+router.post('/notification', isACompany, controller.sendNotification);
 
 module.exports = router;
